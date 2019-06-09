80037de6d30e63dedc1a1c41a991c06213a9a084
Bryon <bryonlh@gmail.com>
2019-06-08 17:16:11 -0700

    Updated the index file and added variables

---
+++ assets/javascript/game.js
@@ -0,0 +1,14 @@
+//variables here
+var crystal1 = '';
+var crystal2 = '';
+var crystal3 = '';
+var crystal4 = '';
+var randomNum = '';
+var score = '';
+
+
+//randomize number for goal 19-120
+
+
+//randomize number for the crystals 1-12
+
